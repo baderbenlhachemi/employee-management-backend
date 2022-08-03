a01d67e0a866cbf84b25cc0f028dc39497fa292c
Bader <baderbenlhachemi@gmail.com>
2022-08-03 14:33:22 +0100

    create company(request & response) + add dependencies

---
+++ src/main/java/com/giantlink/grh/dto/request/CompanyRequest.java
@@ -0,0 +1,22 @@
+package com.giantlink.grh.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CompanyRequest {
+    @NotNull(message = "Company name is required")
+    private String name;
+    @Pattern(regexp = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+    private String email;
+    @NotNull(message = "Company address is required")
+    private String address;
+}
---
+++ src/main/java/com/giantlink/grh/dto/response/CompanyResponse.java
@@ -0,0 +1,17 @@
+package com.giantlink.grh.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CompanyResponse {
+    private Integer id;
+    private String name;
+    private String email;
+    private String address;
+}
